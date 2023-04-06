6948ace7a65c64e0779e62b8970164aab38681d6
AdrienA <aabdalalim5@gmail.com>
2023-04-06 12:33:24 +0200

    modifs pagelog

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,28 +1,48 @@
 import React from "react";
 import './Card.scss';
 // logements
-import logements from "../../assets/logements";
+import logements from "../../assets/logements.json";
 import { Link } from 'react-router-dom'
+//import { useParams, Navigate } from 'react-router-dom'
 
 
 export default function Card() {
+
+<div className="listlog">
+  {logements.map((logement) => {
     return (
-        <>
-
-    <div className="listlog">
-      {logements.map((logements) => {
-        return (
-          <Link key={logements.id} to={`/logement/${logements.id}`}>
-            <div className="log">
-              <img src={logements.cover} alt={logements.title} />
-              <p>{logements.title}</p>
-            </div>
-          </Link>
-        );
-      })}
-    </div>
-
-
-        </>
-    )
+      <Link key={logement.id} to={`/logement/${logement.id}`}>
+        <div className="log">
+        <img src={logement.cover} alt={logement.title} />
+        <p>{logement.title}</p>
+        </div>
+      </Link>
+    );
+})};
+</div>
 }
+
+  /*
+  const { id } = useParams();
+    useEffect( () => {
+
+      const log = logements.find((element)  => element.id === id);
+      console.log(log)
+      console.log(id);
+
+      if(log === undefined){
+        //return(<Navigate to="/" />);
+        console.log("non trouvé")
+      }else {
+        console.log(id);
+        
+        /*
+        return (
+          <div>{log.title}</div>
+        )
+
+     console.log("trouvé");
+      }
+        })
+
+      */
--- src/pages/Logements/Logement.jsx
+++ src/pages/Logements/Logement.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 // Erreur 404
 import Error from "../../components/Error/Error";
 // logements
-import logements from "../../assets/logements";
+import logements from "../../assets/logements.json";
 // etoile note
 import Rate from "../../components/Rate/Rate"
 
@@ -12,51 +12,53 @@ import { Collapse } from 'antd';
 const { Panel } = Collapse;
 
 export default function Pagelog () {
-  const { logementsId } = useParams();
-  const log = logements.find((log) => logements.id === logementsId);
-  console.log(log) ;
+  const { id } = useParams();
+  const log = logements.find((logement) => logement.id === id);
+  console.log(id);
   if (log === undefined || log === null) {
     return <Error />
   }//if
+  else{
  
-return (
-          <div className="container">
-            <div key={log.id} to={`/logement/${log.id}`}></div>
-          <div className='img'>
-            <img src={log.pictures} alt="img" />
-          </div>
-          <div className='flex'>
-            <div>
-              <h1 className="title">{log.title}</h1>
-              <h2 className="location">{log.location}</h2>
-              <div className="tagContainer">
-			        <span className="tagButton">{log.tags.first}</span>
-              <span className="tagButton">{log.tags.second}</span>
-		</div>
+  return (
+            <div className="container">
+              <div key={log.id} to={`/logement/${log.id}`}></div>
+            <div className='img'>
+              <img src={log.pictures} alt="img" />
             </div>
-
-            <div>
-              <div className='flex_2'>
-                <p className='hostname'>{log.host.name}</p>
-                <img className='picture' src={log.host.picture} alt="pict of host" />
+            <div className='flex'>
+              <div>
+                <h1 className="title">{log.title}</h1>
+                <h2 className="location">{log.location}</h2>
+                <div className="tagContainer">
+                <span className="tagButton">{log.tags.first}</span>
+                <span className="tagButton">{log.tags.second}</span>
+      </div>
               </div>
-              <Rate/>
+
+              <div>
+                <div className='flex_2'>
+                  <p className='hostname'>{log.host.name}</p>
+                  <img className='picture' src={log.host.picture} alt="pict of host" />
+                </div>
+                <Rate/>
+              </div>
+
             </div>
 
-          </div>
+            <Collapse className='container_collapse'>
+              <Panel className='container_panel' header="Description" key="1">
+                <p className="description">{log.description}</p>
+              </Panel>
+              <Panel className='container_panel' header="Équipements" key="2">
+                <p className="equipements">{log.equipments}</p>
+              </Panel>
+            </Collapse>
+            </div>
+  )
 
-          <Collapse className='container_collapse'>
-            <Panel className='container_panel' header="Description" key="1">
-              <p className="description">{log.description}</p>
-            </Panel>
-            <Panel className='container_panel' header="Équipements" key="2">
-              <p className="equipements">{log.equipments}</p>
-            </Panel>
-          </Collapse>
-          </div>
-)
-
-}//Pagelog
+  }//Pagelog
+}
 
 /*
 <div className='tags'>{log.tags}</div>
